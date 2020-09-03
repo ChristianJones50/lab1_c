554d52a067d425110909ce68b9ef445490ab7bf1
Christian Jones <cpj5199@psu.edu>
2020-09-03 15:33:36 +0000

    Spell Check

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 
 int main(void) {
-  char *temp = readline("Enter the Temperature in Celsius: ");
+  char *temp = readline("Enter temperature in celsius: ");
   double c = atof(temp);
   double f = (c*1.8)+32;
   printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n", c,f);
